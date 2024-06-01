58f00406a7a1deac50e5f6f56ac8b3c455b53e74
dipali <dipalivdharmale@gmail.com>
2024-06-01 16:43:04 +0530

    add project4

---
+++ account/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+
+class Student(models.Model):
+    roll = models.IntegerField()
+    name = models.CharField(max_length=100)
+    marks = models.FloatField()
+    email = models.EmailField()
---
+++ account/views.py
@@ -0,0 +1,18 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from .forms import StudentForm
+
+#views.py
+def student_view(request):
+    template_name = "account/form.html"
+    
+    if request.method == "POST":
+        form = StudentForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponse('Data saved successfully.')
+    else:
+        form = StudentForm()
+
+    context = {'form': form}
+    return render(request, template_name, context)
